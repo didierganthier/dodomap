db01e8bc6895ebb6f5ea4835de4af9608f856b00
Didier Peran Ganthier <didierganthierperan@gmail.com>
2023-11-22 14:24:31 -0500

    Add stories section

--- lib/main.dart
+++ lib/main.dart
@@ -31,138 +31,213 @@ class MyApp extends StatelessWidget {
           ),
           backgroundColor: Colors.black,
         ),
-        body: ListView.builder(
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                color: Colors.black,
-                child: Column(
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: ListTile(
-                            leading: Container(
-                              height: 50,
-                              width: 50,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                border: Border.all(
-                                  color: Colors.red,
-                                  width: 2,
-                                ),
-                              ),
-                              child: const Padding(
-                                padding: EdgeInsets.all(2.0),
-                                child: CircleAvatar(
-                                  backgroundImage: NetworkImage(
-                                      'https://randomuser.me/api/portraits/men/69.jpg'),
-                                ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2,
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                            height: 10,
+                            width: 10,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              shape: BoxShape.rectangle,
+                              border: Border.all(
+                                color: Colors.white,
+                                width: 2,
                               ),
                             ),
-                            title: const Text(
-                              'traveler007',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                                fontFamily: 'Proxima Nova',
+                            child: const Icon(
+                              Icons.add,
+                              color: Colors.white,
+                            )),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 20,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 3.0),
+                          child: Container(
+                            height: 70,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Colors.red,
+                                width: 2,
                               ),
                             ),
-                            subtitle: const Text(
-                              'At Cayo Levantado',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontFamily: 'Proxima Nova',
+                            child: Padding(
+                              padding: const EdgeInsets.all(2.0),
+                              child: CircleAvatar(
+                                backgroundImage: NetworkImage(
+                                  'https://randomuser.me/api/portraits/men/$index.jpg',
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        const Icon(
-                          Icons.more_vert,
-                          color: Colors.white,
-                        ),
-                      ],
+                        );
+                      },
                     ),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.network(
-                        'https://th.bing.com/th/id/OIP.Ih3ezvnzUBhGtn5x5CUcpAHaE6?rs=1&pid=ImgDetMain',
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 6,
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      color: Colors.black,
+                      child: Column(
                         children: <Widget>[
-                          Container(
-                            height: 50,
-                            width: 80,
-                            child: const Stack(
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: ListTile(
+                                  leading: Container(
+                                    height: 50,
+                                    width: 50,
+                                    decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                        color: Colors.red,
+                                        width: 2,
+                                      ),
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.all(2.0),
+                                      child: CircleAvatar(
+                                        backgroundImage: NetworkImage(
+                                            'https://randomuser.me/api/portraits/men/69.jpg'),
+                                      ),
+                                    ),
+                                  ),
+                                  title: const Text(
+                                    'traveler007',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontFamily: 'Proxima Nova',
+                                    ),
+                                  ),
+                                  subtitle: const Text(
+                                    'At Cayo Levantado',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Proxima Nova',
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const Icon(
+                                Icons.more_vert,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.network(
+                              'https://th.bing.com/th/id/OIP.Ih3ezvnzUBhGtn5x5CUcpAHaE6?rs=1&pid=ImgDetMain',
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
                               children: <Widget>[
-                                Positioned(
-                                  top: 8.0,
-                                  child: CircleAvatar(
-                                    radius: 15.0,
-                                    backgroundImage: NetworkImage(
-                                        'https://randomuser.me/api/portraits/women/68.jpg'),
+                                Container(
+                                  height: 50,
+                                  width: 80,
+                                  child: const Stack(
+                                    children: <Widget>[
+                                      Positioned(
+                                        top: 8.0,
+                                        child: CircleAvatar(
+                                          radius: 15.0,
+                                          backgroundImage: NetworkImage(
+                                              'https://randomuser.me/api/portraits/women/68.jpg'),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        top: 8.0,
+                                        left: 20.0,
+                                        child: CircleAvatar(
+                                          radius: 15.0,
+                                          backgroundImage: NetworkImage(
+                                              'https://randomuser.me/api/portraits/women/69.jpg'),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        top: 8.0,
+                                        left: 40.0,
+                                        child: CircleAvatar(
+                                          radius: 15.0,
+                                          backgroundImage: NetworkImage(
+                                              'https://randomuser.me/api/portraits/men/70.jpg'),
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
-                                Positioned(
-                                  top: 8.0,
-                                  left: 20.0,
-                                  child: CircleAvatar(
-                                    radius: 15.0,
-                                    backgroundImage: NetworkImage(
-                                        'https://randomuser.me/api/portraits/women/69.jpg'),
+                                const Text(
+                                  'Liked by John, Jane, and 500 others',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontFamily: 'Proxima Nova',
                                   ),
                                 ),
-                                Positioned(
-                                  top: 8.0,
-                                  left: 40.0,
-                                  child: CircleAvatar(
-                                    radius: 15.0,
-                                    backgroundImage: NetworkImage(
-                                        'https://randomuser.me/api/portraits/men/70.jpg'),
-                                  ),
-                                ),
+                                Row(
+                                  children: [
+                                    Image.asset(
+                                      'images/send.png',
+                                      height: 25,
+                                      width: 25,
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    Image.asset(
+                                      'images/bookmark.png',
+                                      height: 25,
+                                      width: 25,
+                                    ),
+                                  ],
+                                )
                               ],
                             ),
                           ),
-                          const Text(
-                            'Liked by John, Jane, and 500 others',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              fontFamily: 'Proxima Nova',
-                            ),
-                          ),
-                          Row(
-                            children: [
-                              Image.asset(
-                                'images/send.png',
-                                height: 25,
-                                width: 25,
-                              ),
-                              const SizedBox(
-                                width: 10,
-                              ),
-                              Image.asset(
-                                'images/bookmark.png',
-                                height: 25,
-                                width: 25,
-                              ),
-                            ],
-                          )
                         ],
                       ),
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
-            );
-          },
+            ),
+          ],
         ),
       ),
     );
